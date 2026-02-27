7ea91f264904b3941870be4da53654c612b22b95
Carl Jonsson <carl.jonsson@student.howest.be>
2026-02-27 15:59:17 +0100

    feat: Board movement for moving left, right and diagonal

--- board_movement.py
+++ board_movement.py
@@ -26,3 +26,42 @@ class BoardMovement:
             return None
 
         return f"{column}{new_row}"
+    
+    @staticmethod
+    def left(position: str, color: str, squares: int = 1):
+        column = position[0]
+        row = position[1]
+
+        new_col = chr(ord(column) - squares)
+
+        if new_col < "a" or new_col > "h":
+            return None
+
+        return f"{new_col}{row}"
+
+    @staticmethod
+    def right(position: str, color: str, squares: int = 1):
+        column = position[0]
+        row = position[1]
+
+        new_col = chr(ord(column) + squares)
+
+        if new_col < "a" or new_col > "h":
+            return None
+
+        return f"{new_col}{row}"
+
+    @staticmethod
+    def diagonal(position: str, color: str, col_dir: int, row_dir: int, squares: int = 1):
+        column = position[0]
+        row = int(position[1])
+
+        new_col = chr(ord(column) + col_dir * squares)
+        new_row = row + row_dir * squares
+
+        if new_col < "a" or new_col > "h":
+            return None
+        if new_row < 1 or new_row > 8:
+            return None
+
+        return f"{new_col}{new_row}"
