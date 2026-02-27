f03e599cf8a5166c138c2d56f264a48a2657a4f2
Carl Jonsson <carl.jonsson@student.howest.be>
2026-02-27 15:51:19 +0100

    feat: Board movement for moving backward

--- board_movement.py
+++ board_movement.py
@@ -13,3 +13,16 @@ class BoardMovement:
             return None
 
         return f"{column}{new_row}"
+
+    @staticmethod
+    def backward(position: str, color: str, squares: int = 1):
+        column = position[0]
+        row = int(position[1])
+
+        direction = 1 if color == "WHITE" else -1
+        new_row = row + (direction * squares)
+
+        if new_row < 1 or new_row > 8:
+            return None
+
+        return f"{column}{new_row}"
