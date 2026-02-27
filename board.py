122ee6105629edee40f125a82ac28406ae31a620
Carl Jonsson <carl.jonsson@student.howest.be>
2026-02-27 14:57:12 +0100

    feat: board setup for pawns, white team, and print_board method

--- board.py
+++ board.py
@@ -19,4 +19,37 @@ class Board:
         self.squares["f1"] = Bishop("BLACK", 2)
         self.squares["g1"] = Knight("BLACK", 2)
         self.squares["h1"] = Rook("BLACK", 2)
+
+
+        black_pawns = {
+            f"{file}2": Pawn("BLACK", idx)
+            for idx, file in enumerate("abcdefgh", start=1)
+        }
+        self.squares.update(black_pawns)
+
+        white_pawms = {
+            f"{file}7": Pawn("WHITE", idx)
+            for idx, file in enumerate("abcdefgh", start=1)
+        }
+        self.squares.update(white_pawms)
+
+        self.squares["a8"] = Rook("WHITE", 1)
+        self.squares["b8"] = Knight("WHITE", 1)
+        self.squares["c8"] = Bishop("WHITE", 1)
+        self.squares["d8"] = Queen("WHITE", 1)
+        self.squares["e8"] = King("WHITE", 1)
+        self.squares["f8"] = Bishop("WHITE", 2)
+        self.squares["g8"] = Knight("WHITE", 2)
+        self.squares["h8"] = Rook("WHITE", 2)
+
+    def print_board(self):
+        rows = []
+        for row in range(1, 9):
+            row_pieces = []
+            for col in range(ord("a"), ord("i")):
+                square = f"{chr(col)}{row}"
+                row_pieces.append(self.squares[square])
+            rows.append(row_pieces)
         
+        for row in rows:
+            print(row)
