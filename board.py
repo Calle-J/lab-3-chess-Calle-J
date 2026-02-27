796d1dd37e8e5ebc4e3933bda0b33981be5d6903
Carl Jonsson <carl.jonsson@student.howest.be>
2026-02-27 14:46:28 +0100

    feat: board setup for the first row of the black team

--- board.py
+++ board.py
@@ -9,3 +9,14 @@ class Board:
             for col in range(ord("a"), ord("i"))
             for row in range(1, 9)
         }
+    
+    def setup_board(self):
+        self.squares["a1"] = Rook("BLACK", 1)
+        self.squares["b1"] = Knight("BLACK", 1)
+        self.squares["c1"] = Bishop("BLACK", 1)
+        self.squares["d1"] = Queen("BLACK", 1)
+        self.squares["e1"] = King("BLACK", 1)
+        self.squares["f1"] = Bishop("BLACK", 2)
+        self.squares["g1"] = Knight("BLACK", 2)
+        self.squares["h1"] = Rook("BLACK", 2)
+        
