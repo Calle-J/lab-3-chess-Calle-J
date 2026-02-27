7d5bfbe3b44d041f2147c3f8199811a7cc4fdeba
Carl Jonsson <carl.jonsson@student.howest.be>
2026-02-27 14:30:53 +0100

    feat: constructor in new Board class with dict comprehension creating all sqaures a1-h8 with value None

---
+++ board.py
@@ -0,0 +1,11 @@
+from pieces import Pawn, Rook, Bishop, Knight, Queen, King
+
+class Board:
+    def __init__(self):
+        # Dict comprehension: Creates all squares a1-h8 with value None.
+
+        self.squares = {
+            f"{chr(col)}{row}": None
+            for col in range(ord("a"), ord("i"))
+            for row in range(1, 9)
+        }
